48d1f39a1be11d9ea6865b023e611b1f2d97e904
Esteban Andrés Piña Cabañas <39716335+EstebanPina@users.noreply.github.com>
2026-03-04 21:44:49 -0600

    [del]: Deleting amplify cognito dependencies

--- amplify/backend.ts
+++
@@ -1,9 +0,0 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-
-// Solo autenticación con Cognito - el backend REST permanece en Next.js
-const backend = defineBackend({
-  auth
-});
-
-export default backend;
